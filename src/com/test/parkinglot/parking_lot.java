4996996c0eb080b09728db4186a5a6ef6dc4ecf5
Sigit Ari <sigit_saw@ymail.com>
2017-05-20 12:24:37 +0700

    menambahkan main program

---
+++ src/com/test/parkinglot/parking_lot.java
@@ -0,0 +1,10 @@
+package com.test.parkinglot;
+
+/**
+ * Created by Sigit on 20/05/2017.
+ */
+public class parking_lot {
+    public static void main(String[] args) {
+        // write your code here
+    }
+}
