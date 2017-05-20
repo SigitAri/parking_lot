520ea8bce581b948d8bc91f47b958557d791e234
Sigit Ari <sigit_saw@ymail.com>
2017-05-20 12:53:33 +0700

    menambahkan class main parking_lot serta menjalankan program

--- src/com/test/parkinglot/parking_lot.java
+++ src/com/test/parkinglot/parking_lot.java
@@ -1,10 +1,20 @@
 package com.test.parkinglot;
 
+import com.test.parkinglot.core.ParkingIntruction;
+
+import java.util.Scanner;
+
 /**
  * Created by Sigit on 20/05/2017.
  */
 public class parking_lot {
     public static void main(String[] args) {
-        // write your code here
+        ParkingIntruction parkingIntruction = new ParkingIntruction();
+        Scanner reader = new Scanner(System.in);
+        while (true) {
+            String intruction = reader.nextLine();
+            String message = parkingIntruction.Intruction(intruction);
+            System.out.println(message);
+        }
     }
 }
